b52038883a2b4daaafa642c280ab54435fe4cb72
FernandoPucci <fsilvapucci@gmail.com>
2017-10-08 20:18:24 -0300

    class Binding

--- hello-world/src/app/courses.component.ts
+++ hello-world/src/app/courses.component.ts
@@ -20,7 +20,7 @@ import { Component } from '@angular/core';
             </tr>
         </table>
 
-    <button class="btn btn-primary">Save</button>
+    <button class="btn btn-primary" [class.active]="active">Save</button>
     `
 })
 
@@ -30,6 +30,7 @@ export class CoursesComponent {
     authors;
     imageUrl = "http://lorempixel.com/400/200";
     colSpan = 2;
+    active = false;
 
     constructor(service: CoursesService, authorService: AuthorsService) {
         this.courses = service.getCourses();
